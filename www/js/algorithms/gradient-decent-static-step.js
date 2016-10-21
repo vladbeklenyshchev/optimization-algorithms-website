cf6b3cba6cdfdae6e52147c93bac80ab50de35cf
vlad <vladbeklenyshchev@gmail.com>
2016-10-21 22:00:41 +0300

    Added first solution of gradient decent method
    it has a bug with cycle :(

--- www/js/algorithms/gradient-decent-static-step.js
+++ www/js/algorithms/gradient-decent-static-step.js
@@ -1,43 +1,40 @@
-var main = function(x, eps1, eps2, M){
-	var resulted_arr_x = [x];
+//test - calcGradientDescentWithStaticStep([0.5, 1], 0.1, 0.15, 10)
+var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M){
+	// x0 - vector
+	var x = [x0];
+	
 	k = -1
-	grad_fxk = [0,0];// calculate
-	tk = 1; // static!
-	do
-	{
+	grad_val = [0,0];// calculate
+	next_item = [0, 0];
+	func_x = [];
+	tk = 1.0; // static!
+	
+	do{
 		k = k + 1;
-
-		grad_fxk = grad_f_x(x[0],x[1]);
-
-		if(grad_fxk < eps1 || k>=M){
-			return x;
+		item = x[k]
+		grad_val = grad_f_x(item[0], item[1]);
+		console.log("grad_val = " + grad_val)
+		if(norm1(grad_val[0], grad_val[1]) < eps1){
+			return x[k]
 		}
-		
-		x_next[0] = x[0] - tk * grad_fxk[0];
-		x_next[1] = x[1] - tk * grad_fxk[1];
+		else if(k>=M){
+			return x[k]
+		}
+		do{
+			next_item[0] = item[0] - tk * grad_val[0];
+			next_item[1] = item[1] - tk * grad_val[1];
+			console.log("x = " + x)
+			tk = tk / 2.0;
+			func_x[k] = f_x(item[0], item[1])
+			func_x[k+1] = f_x(next_item[0], next_item[1])
+			
+		}while(func_x[k+1] >= func_x[k])
+		item = next_item
+		x.push(item)
+		console.log(norm2(x[k],x[k+1]))
+		console.log(func_x[k+1] - func_x[k])
+	}while(norm2(x[k],x[k+1])>=eps2 
+	|| abs(func_x[k+1] - func_x[k])>=eps2)
 
-	}while(abs(arr_x[k+1] - arr_x[k])<eps2 
-		&& abs(func_x[k+1] - func_x[k])<eps1)
-}
-
-var abs = function(a){
-	if(a < 0)
-		return -a;
-	else return a;
-}
-
-var norm = function(x1,x2){
-	return Math.sqrt((x2[0] - x1[0]) * (x2[0] - x1[0])
-		+ (x2[1] - x1[1]) * (x2[1] - x1[1]));
-}
-
-var f_x = function(x1, x2){
-	return 2*x1*x1 + x1*x2 + x2*x2;
-}
-
-var grad_f_x = function(x1, x2){
-	arr_grad = [0,0]
-	arr_grad[0] = 4 * x1 + x2;
-	arr_grad[1] = x1 + 2 * x2;
-	return arr_grad;
+	return x[k]
 }
