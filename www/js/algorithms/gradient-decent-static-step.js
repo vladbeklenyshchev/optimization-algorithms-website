4b7e85d8dab55faa6de486a7f631a742fae7bd53
vlad <vladbeklenyshchev@gmail.com>
2016-10-25 19:19:19 +0300

    Bug was eliminated and method gradient descent was done
    to eliminate bug use new function "action" I created

--- www/js/algorithms/gradient-decent-static-step.js
+++ www/js/algorithms/gradient-decent-static-step.js
@@ -1,6 +1,5 @@
 //test - calcGradientDescentWithStaticStep([0.5, 1], 0.1, 0.15, 10)
  var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M){
-	"use strict";
 	// x0 - vector
 	var x = [x0];
 	k = 0
@@ -11,28 +10,16 @@
 	tk = 0.5;
 	do{
 		grad_val = grad_f_x(item[0], item[1]);
-		console.log("grad_val = " + grad_val)
 		if(norm1(grad_val[0], grad_val[1]) < eps1){
-			return x[k]
+			return x[k];
 		}
 		else if(k>=M){
-			return x[k]
+			return x[k];
 		}
-		console.log("x_k111 = " + item)
 
 		do{
-			console.log("x_k = " + item)
-			var tmp = item
-			// item становится next_item раньше времени!!!!
-			next_item[0] = item[0] - tk * grad_val[0];
-			console.log("x_k_whaaaat = " + tmp)
-			next_item[1] = item[1] - tk * grad_val[1];
-			item = tmp
-			console.log("tk = " + tk)
-			console.log("x_k = " + item)
-			console.log("x_k+1 = " + next_item)
-			console.log("f_k = " + (f_x(item[0], item[1])))
-			console.log("f_k+1 = " + (f_x(next_item[0], next_item[1])))
+			// to resolve proble with asinc - create new function
+			next_item = action(item, grad_val, tk);
 			condition = ((f_x(next_item[0], next_item[1]) - f_x(item[0], item[1])) > 0)
 			if(condition){
 				tk = tk / 2.0;
@@ -40,10 +27,15 @@
 		}while(condition)
 		item = next_item
 		x.push(item)
-		/*console.log(norm2(x[k],x[k+1]))
-		console.log(func_x[k+1] - func_x[k])*/
 		k = k + 1;
 	}while(norm2(x[k-1],x[k])>eps2)
 
 	return x[k]
-}
+};
+
+var action = function(x, grad, alpha){
+	var y = [0,0]
+	y[0] = x[0] - alpha * grad[0];
+	y[1] = x[1] - alpha * grad[1];
+	return y;
+};
