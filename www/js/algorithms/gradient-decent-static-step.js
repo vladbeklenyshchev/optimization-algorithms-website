13eebe1ffc795c52ec84ff5b529b7a7dd85d24c5
vlad <vladbeklenyshchev@gmail.com>
2016-10-25 19:08:20 +0300

    Found the bug
    // item становится next_item раньше времени!!!!
    			next_item[0] = item[0] - tk * grad_val[0];
    			console.log("x_k_whaaaat = " + tmp)
    			next_item[1] = item[1] - tk * grad_val[1];

--- www/js/algorithms/gradient-decent-static-step.js
+++ www/js/algorithms/gradient-decent-static-step.js
@@ -1,17 +1,15 @@
 //test - calcGradientDescentWithStaticStep([0.5, 1], 0.1, 0.15, 10)
-var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M){
+ var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M){
+	"use strict";
 	// x0 - vector
 	var x = [x0];
-	
-	k = -1
-	grad_val = [0,0];// calculate
-	next_item = [0, 0];
+	k = 0
+	grad_val = [0,0];
+	item = x0
+	next_item = [0,0];
 	func_x = [];
-	tk = 1.0; // static!
-	
+	tk = 0.5;
 	do{
-		k = k + 1;
-		item = x[k]
 		grad_val = grad_f_x(item[0], item[1]);
 		console.log("grad_val = " + grad_val)
 		if(norm1(grad_val[0], grad_val[1]) < eps1){
@@ -20,21 +18,32 @@ var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M){
 		else if(k>=M){
 			return x[k]
 		}
+		console.log("x_k111 = " + item)
+
 		do{
+			console.log("x_k = " + item)
+			var tmp = item
+			// item становится next_item раньше времени!!!!
 			next_item[0] = item[0] - tk * grad_val[0];
+			console.log("x_k_whaaaat = " + tmp)
 			next_item[1] = item[1] - tk * grad_val[1];
-			console.log("x = " + x)
-			tk = tk / 2.0;
-			func_x[k] = f_x(item[0], item[1])
-			func_x[k+1] = f_x(next_item[0], next_item[1])
-			
-		}while(func_x[k+1] >= func_x[k])
+			item = tmp
+			console.log("tk = " + tk)
+			console.log("x_k = " + item)
+			console.log("x_k+1 = " + next_item)
+			console.log("f_k = " + (f_x(item[0], item[1])))
+			console.log("f_k+1 = " + (f_x(next_item[0], next_item[1])))
+			condition = ((f_x(next_item[0], next_item[1]) - f_x(item[0], item[1])) > 0)
+			if(condition){
+				tk = tk / 2.0;
+			}
+		}while(condition)
 		item = next_item
 		x.push(item)
-		console.log(norm2(x[k],x[k+1]))
-		console.log(func_x[k+1] - func_x[k])
-	}while(norm2(x[k],x[k+1])>=eps2 
-	|| abs(func_x[k+1] - func_x[k])>=eps2)
+		/*console.log(norm2(x[k],x[k+1]))
+		console.log(func_x[k+1] - func_x[k])*/
+		k = k + 1;
+	}while(norm2(x[k-1],x[k])>eps2)
 
 	return x[k]
 }
