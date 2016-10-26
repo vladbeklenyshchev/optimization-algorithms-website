e14720123a5aaaf9f1c278d90be1a65926880d6b
vlad <vladbeklenyshchev@gmail.com>
2016-10-26 20:31:11 +0300

    Added semicolons for gradient-descent and math functions
    Yes, semicolon :D

--- www/js/algorithms/all_math_functions.js
+++ www/js/algorithms/all_math_functions.js
@@ -2,26 +2,26 @@ var abs = function(a){
 	if(a < 0)
 		return -a;
 	else return a;
-}
+};
 
  var f_x = function(x1, x2){
 	return 2*x1*x1 + x1*x2 + x2*x2;
-}
+};
 
 
   var grad_f_x = function(x1, x2){
-	arr_grad = [0,0]
+	arr_grad = [0,0];
 	arr_grad[0] = 4 * x1 + x2;
 	arr_grad[1] = x1 + 2 * x2;
 	return arr_grad;
-}
+};
 
  // norm1 for one vector with two elements
 var norm1 = function(item1, item2) {
 	return Math.sqrt(item1*item1 + item2*item2);
-}
+};
 
  // norm2 for two vectors with two elements
 var norm2 = function(x1, x2) {
 	return Math.sqrt((x2[0] - x1[0]) * (x2[0] - x1[0])+ (x2[1] - x1[1]) * (x2[1] - x1[1]));
-}
+};
--- www/js/algorithms/gradient-decent-static-step.js
+++ www/js/algorithms/gradient-decent-static-step.js
@@ -1,9 +1,9 @@
 //test - calcGradientDescentWithStaticStep([0.5, 1], 0.1, 0.15, 10)
  var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M){
 	var x = [x0];
-	k = 0
+	k = 0;
 	grad_val = [0,0];
-	item = x0
+	item = x0;
 	next_item = [0,0];
 	func_x = [];
 	tk = 0.5;
@@ -17,23 +17,22 @@
 		}
 
 		do{
-			// to resolve proble with asinc - create new function
 			next_item = action(item, grad_val, tk);
-			condition = ((f_x(next_item[0], next_item[1]) - f_x(item[0], item[1])) > 0)
+			condition = ((f_x(next_item[0], next_item[1]) - f_x(item[0], item[1])) > 0);
 			if(condition){
 				tk = tk / 2.0;
 			}
 		}while(condition)
-		item = next_item
-		x.push(item)
+		item = next_item;
+		x.push(item);
 		k = k + 1;
 	}while(norm2(x[k-1],x[k])>eps2)
 
-	return x[k]
+	return x[k];
 };
 
 var action = function(x, grad, alpha){
-	var y = [0,0]
+	var y = [0,0];
 	y[0] = x[0] - alpha * grad[0];
 	y[1] = x[1] - alpha * grad[1];
 	return y;
