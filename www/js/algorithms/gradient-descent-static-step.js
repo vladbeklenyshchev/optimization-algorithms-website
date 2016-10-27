f8ace3073d25a8877a2e566cc115e7c4caa4f44b
vlad <vladbeklenyshchev@gmail.com>
2016-10-27 11:30:33 +0300

    Added white spaces to be closer to JavaScript style :)
    Everywhere :)

--- www/js/algorithms/gradient-descent-static-step.js
+++ www/js/algorithms/gradient-descent-static-step.js
@@ -1,5 +1,5 @@
 //test - calcGradientDescentWithStaticStep([0.5, 1], 0.1, 0.15, 10)
-var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M){
+var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M) {
 	var x = [x0];
 	var k = 0;
 	var grad_val = [0,0];
@@ -11,32 +11,31 @@ var calcGradientDescentWithStaticStep = function(x0, eps1, eps2, M){
 	do{
 		grad_val = grad_f_x(item[0], item[1]);
 		
-		if(norm1(grad_val[0], grad_val[1]) < eps1){
+		if (norm1(grad_val[0], grad_val[1]) < eps1) {
 			return x[k];
-		}
-		else if(k>=M){
+		} else if(k>=M) {
 			return x[k];
 		}
 
-		do{
+		do {
 			next_item = [0,0]
 			next_item = action(item, grad_val, tk);
 			condition = ((f_x(next_item[0], next_item[1]) - f_x(item[0], item[1])) > 0);
-			if(condition){
+			if(condition) {
 				tk = tk / 2.0;
 			}
-		}while(condition);
+		} while(condition);
 
 		item = next_item;
 		x.push(item);
 		k = k + 1;
 
-	}while(norm2(x[k-1],x[k])>eps2);
+	} while(norm2(x[k-1],x[k]) > eps2);
 
 	return x[k];
 };
 
-var action = function(x, grad, alpha){
+var action = function(x, grad, alpha) {
 	var y = [0, 0];
 	y[0] = x[0] - alpha * grad[0];
 	y[1] = x[1] - alpha * grad[1];
--- www/js/algorithms/math-functions.js
+++ www/js/algorithms/math-functions.js
@@ -1,14 +1,16 @@
-var abs = function(a){
-	if(a < 0)
+var abs = function(a) {
+	if(a < 0) {
 		return -a;
-	else return a;
+	} else {
+		return a;
+	}
 };
 
- var f_x = function(x1, x2){
+ var f_x = function(x1, x2) {
 	return 2*x1*x1 + x1*x2 + x2*x2;
 };
 
- var grad_f_x = function(x1, x2){
+ var grad_f_x = function(x1, x2) {
 	arr_grad = [0,0];
 	arr_grad[0] = 4 * x1 + x2;
 	arr_grad[1] = x1 + 2 * x2;
@@ -22,6 +24,6 @@ var norm1 = function(item1, item2) {
 
  // norm2 for two vectors with two elements
 var norm2 = function(x1, x2) {
-	return Math.sqrt((x2[0] - x1[0]) * (x2[0] - x1[0])
-		+(x2[1] - x1[1]) * (x2[1] - x1[1]));
+	return Math.sqrt((x2[0] - x1[0]) * (x2[0] - x1[0]) + 
+		(x2[1] - x1[1]) * (x2[1] - x1[1]));
 };
