7004957c3fa4dad9077c81eb872fe06f867e64ab
TanyaPilipenko <tanjap1994@gmail.com>
2016-10-27 18:29:04 +0300

    New changes (created G-S-m / changed index and m-steepest-grad-descent)
    1) created Gauss-Seidel-method (but didn't create step for method)
    2) changed method-of-steepest-gradient-descent:
    - changed break; to return
    - variables set to zero
    - used functions from file math-functions.js
    - adhered to style JS
    3) changed index.html (formated code)

--- www/js/algorithms/method-of-steepest-gradient-descent.js
+++ www/js/algorithms/method-of-steepest-gradient-descent.js
@@ -18,89 +18,65 @@
         var result = new Array(2);
         var result_function = 0;
         // ГЛАВНЫЙ ЦИКЛ
-    	while(norm >= e1 || norm_difference >= e2)
-    	{
+    	while(norm >= e1 || norm_difference >= e2) {
     		// ВЫЧИСЛЯЕМ ГРАДИЕНТ В ТОЧКЕ Х0
 			var grad = new Array(2); //градиент
-			grad[0] = gradX(x0[0], x0[1]);  //градиенты в точке Х0
-        	grad[1] = gradY(x0[0], x0[1]);
+			grad = [0, 0];  //градиенты в точке Х0
+            grad = grad_f_x(x0[0], x0[1]);
         	// Вычисляем норму в точке Х0
-        	norm = norm(grad[0], grad[1]);
-        	// ПРОВЕРЯЕМ УСЛОВИЕ norm (grad) < e1
-        	if (norm < e1)
-        	{
+            norm = 0;
+        	norm = norm1(grad[0], grad[1]);
+        	// ПРОВЕРЯЕМ УСЛОВИЕ norm1 (grad) < e1
+        	if (norm < e1) {
+                result = [0, 0];
         		result[0] = x0[0];
         		result[1] = x0[1];
-        		result_function = f(result[0], result[1]);
-        		break;
-        	}
-        	else
-        	{
+                result_function = 0;
+        		result_function = f_x(result[0], result[1]);
+        		return result;
+        	} else {
 	        	// ПРОВЕРЯЕМ УСЛОВИЕ k>=M
-	        	if(k >= m)
-	        	{
+	        	if(k >= m) {
+                    result = [0, 0];
         			result[0] = x0[0];
         			result[1] = x0[1];
-        			result_function = f(result[0], result[1]);
-	        		break;
-	        	}
-	        	else
-	        	{
+                    result_function = 0;
+        			result_function = f_x(result[0], result[1]);
+	        		return result;
+	        	} else {
 	        		var x1 = new Array(2);
 	        		var t = 0;
 	        		// ВЫЧИСЛЯЕМ НАИЛУЧШУЮ ВЕЛИЧИНУ ШАГА t
 	        		t = step(x0[0], x0[1], k);
+                    x1 = [0, 0];
 	        		x1[0] = x0[0] - grad[0]*t;
 	        		x1[1] = x0[1] - grad[1]*t;
 	        		// ВЫЧИСЛЯЕМ НОРМУ РАЗНОСТИ КООРДИНАТ
-	        		norm_difference = norm_difference(x0, x1);
-	        		if (norm_difference < e2) 
-	        		{
+                    norm_difference = 0;
+	        		norm_difference = norm2(x0, x1);
+	        		if (norm_difference < e2) {
+                        result = [0, 0];
 	        			result[0] = x1[0];
 	        			result[1] = x1[1];
-	        			result_function = f(result[0], result[1]);
-	        			break;
-	        		}
-	        		else
-	        		{
+                        result_function = 0;
+	        			result_function = f_x(result[0], result[1]);
+	        			return result;
+	        		} else {
 	        			k++;
 	        			// ВЫЧИСЛЯЕМ ГРАДИЕНТ В ТОЧКЕ Х0
+                        x0 = [0, 0];
 	        			x0[0] = x1[0];
 	        			x0[1] = x1[1];
 	        		}
         		}
     		}
         }
-        alert("точка минимума = [" + result[1].toFixed(5)+", " + result[1].toFixed(5) +"], значение функции в этой точке " + result_function.toFixed(5));
+        // alert("точка минимума = [" + result[1].toFixed(5)+", " + result[1].toFixed(5) +"], значение функции в этой точке " + result_function.toFixed(5));
     }
 
-function f(x, y)
-{
-    var f = 2*x*x + x*y + y*y; // заданная квадратичная форма
-    return f;
-}
-
-function gradX(x, y) // градиент по Х
-{
-    var gradX = 4*x + y;
-    return gradX;
-}
-
-function gradY(x, y) // градиент по Y
-{
-    var gradY = x + 2*y;
-    return gradY;
-}
-
-function norm(x, y) // норма
-{ 
-	var norm = Math.sqrt(x*x + y*y);
-  	return norm;
-} 
-
-function step(x, y, k) // шаг
-{ 
-	var step = ( Math.pow((4*Math.pow(x, k) + Math.pow(y, k)), 2) 
+function step(x, y, k) { // шаг
+	var step = 0;
+    step = ( Math.pow((4*Math.pow(x, k) + Math.pow(y, k)), 2) 
 				+ Math.pow((Math.pow(x, k) + 2*Math.pow(y, k)), 2) ) 
 				/ (4*Math.pow((4*Math.pow(x,k) + Math.pow(y, k)), 2) 
 				+ 2*(4*Math.pow(x, k) + Math.pow(y, k))
@@ -108,9 +84,3 @@ function step(x, y, k) // шаг
 				+ 2* Math.pow((Math.pow(x, k) + 2*Math.pow(y, k)), 2));
   	return step;
 }
-
-function norm_difference(x1, x2) //норма разности координат
-{
-	var  norm_difference = Math.sqrt((x2[0] - x1[0]) * (x2[0] - x1[0]) + (x2[1] - x1[1]) * (x2[1] - x1[1]));
-  	return norm_difference;
-} 
