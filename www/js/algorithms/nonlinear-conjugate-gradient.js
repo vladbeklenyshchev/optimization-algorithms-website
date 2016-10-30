6af4cb8edf978e37e36ba51e32f61d35c0d17b0a
vlad <vladbeklenyshchev@gmail.com>
2016-10-30 18:13:08 +0200

    Complete nonlinear-conjugate method

--- www/js/algorithms/nonlinear-conjugate-gradient.js
+++ www/js/algorithms/nonlinear-conjugate-gradient.js
@@ -1,5 +1,6 @@
 //test - calcNonlinearConjugateGradient([0.5, 1], 0.1, 0.15, 10)
 var calcNonlinearConjugateGradient = function(x0, eps1, eps2, M) {
+	"use strict";
 	var x = [x0];
 	var k = 0;
 	var grad_values = [];
@@ -10,6 +11,7 @@ var calcNonlinearConjugateGradient = function(x0, eps1, eps2, M) {
 	// matrix
 	var d = [];
 	var beta = 0.0;
+	var d1 = 0, d2 = 0;
 
 	do{
 		grad_values[k] = grad_f_x(x[k][0], x[k][1]);
@@ -19,19 +21,18 @@ var calcNonlinearConjugateGradient = function(x0, eps1, eps2, M) {
 		} else if(k >= M) {
 			return x[k];
 		} else if(k == 0){
-			debugger;
 			d.push(grad_f_x(-x0[0], -x0[1]));
 			t = step(x0[0], x0[1], k); // 0.24
 			x.push( action(x0, t, d[k]) );
 		} else {
-			debugger;
 			beta = (Math.pow(norm1(grad_values[k][0], grad_values[k][1]), 2) /
 			Math.pow(norm1(grad_values[k-1][0], grad_values[k-1][1]), 2));
-			d.push(grad_f_x(- x[k][0], - x[k][1]) + beta * d[k-1]);
+			d1 = - grad_values[k][0] + beta * d[k-1][0];
+			d2 = - grad_values[k][1] + beta * d[k-1][1];
+			d.push([d1, d2]);
 			t = step(x[k][0], x[k][1], k);
-			x.push( action(x[k-1], t, [d[k][0].toFixed(5), d[k][1].toFixed(5)] ));
+			x.push( action(x[k], t, d[k] ));
 		}
-		debugger;
 		k = k + 1;
 		// used only one stop condition
 		// additional: abs(f_x(next_item[0], next_item[1]) - 
@@ -51,11 +52,11 @@ var action = function(x, t, d) {
 
 function step(x, y, k) { // шаг
 	var step = 0;
-    step = ( Math.pow((4*Math.pow(x, k) + Math.pow(y, k)), 2) 
-				+ Math.pow((Math.pow(x, k) + 2*Math.pow(y, k)), 2) ) 
-				/ (4*Math.pow((4*Math.pow(x,k) + Math.pow(y, k)), 2) 
-				+ 2*(4*Math.pow(x, k) + Math.pow(y, k))
-				*(Math.pow(x, k) + 2*Math.pow(y, k)) 
-				+ 2* Math.pow((Math.pow(x, k) + 2*Math.pow(y, k)), 2));
+    step = (Math.pow(4*x + y, 2) 
+				+ Math.pow(x + 2*y, 2) ) 
+				/ (4*Math.pow(4*x + y, 2) 
+				+ 2*(4*x + y)
+				*(x + 2*y) 
+				+ 2* Math.pow(x + 2*y, 2));
   	return step;
 }
