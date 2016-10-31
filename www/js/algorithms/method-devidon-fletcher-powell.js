432cfc601da9bd78a8bf854fefa4d3f92464440e
TanyaPilipenko <tanjap1994@gmail.com>
2016-10-31 13:56:37 +0200

    method-devidon-fletcher-powell is almost ready. It worked correctly.
    But I didn't write formula to calculate matrix A. Now I wrote matrix A in code.

--- www/js/algorithms/method-devidon-fletcher-powell.js
+++ www/js/algorithms/method-devidon-fletcher-powell.js
@@ -15,23 +15,24 @@ function method_6(obj)
 			var a = [[1, 0], [0, 1]]; // Единичная начальная матрица А
 			var a1 = [[1, 0], [0, 1]]; // Новая матрица А, которая вычисляется страшной формулой
 			var a_new = [[0, 0], [0, 0]]; // Новая матрица А = а + а1
-			var d = []; // Направление
+			var d = [0, 0]; // Направление
 			var delta_g = [0, 0]; 
 			var delta_x = [0, 0];
 			var t = 0; // шаг
-            var x1 = new Array(2); 
+            var x1 = [0, 0]; 
     	    // Дополнительные переменные
     	    var norm = 100;
     	    var norm_difference = 100;
     	    var norm_difference_f_x = 100;
-            var result = new Array(2);
-            var result_function = 0;   
+            var result = [0, 0];
+            var result_function = 0;
+            var grad = [0, 0]; //градиент в точке Х0
+            var grad2 = [0, 0]; //градиенты в точке Х1
             // ГЛАВНЫЙ ЦИКЛ
     	    while(norm >= e1 || norm_difference >= e2 || norm_difference_f_x >= e2) {
     		    // ПРОВЕРЯЕМ УСЛОВИЕ k>=M
     		    if (k <= m) {
     				// ВЫЧИСЛЯЕМ ГРАДИЕНТ В ТОЧКЕ Х0
-					var grad = new Array(2); //градиент
 					grad = [0, 0];  //градиенты в точке Х0
 	            	grad = grad_f_x(x0[0], x0[1]);
 	            	// Вычисляем норму в точке Х0
@@ -47,13 +48,13 @@ function method_6(obj)
 		        		return result;
 		        	} else {
 		        		if (k != 0) {
-		        			var grad2 = [0, 0];  //градиенты в точке Х1
+		        			grad2 = [0, 0];  //градиенты в точке Х1
 	            			grad2 = grad_f_x(x1[0], x1[1]);
 	            			// Вычисляем норму в точке Х1
 					        norm = 0;
 					        norm = norm1(grad2[0], grad2[1]);
 					        // ПРОВЕРЯЕМ УСЛОВИЕ norm1 (grad) < e1
-					        if (norm < e1) {
+					        if (norm > e1) {
 					        	delta_g[0] = grad2[0] - grad[0];
 					        	delta_g[1] = grad2[1] - grad[1];
 					        	delta_x[0] = x1[0] - x0[0];
@@ -75,15 +76,26 @@ function method_6(obj)
 					        }
 		        		}
 		        		// ВЫЧИСЛЯЕМ d
-		        		d = multMatrixNumber(-1, MultiplyMatrix(a1, grad));
+		        		if(k == 0){
+		        			d = multMatrixNumber(-1, MultiplyMatrix(a1, grad));
+		        		} else {
+		        			d = multMatrixNumber(-1, MultiplyMatrix(a1, grad2));
+		        		}
 		        		// ВЫЧИСЛЯЕМ ШАГ t*
-		        		x0 = [0, 0];
-		        		x0[0] = x1[0];
-		        		x0[1] = x1[1];
-		        		t = step(x0, grad);
-		        		x1 = [0, 0];
-                        x1[0] = x0[0] - grad[0] * t;
-                        x1[1] = x0[1] - grad[1] * t;
+		        		if(k != 0){
+		        			x0 = [0, 0];
+		        			x0[0] = x1[0];
+		        			x0[1] = x1[1];
+		        			t = step(x0, grad, k, d);
+		        			x1 = [0, 0];
+                        	x1[0] = x0[0] - d[0] * t;
+                        	x1[1] = x0[1] - d[1] * t;
+		        		} else {
+		        			t = step(x0, grad, k, d);
+		        			x1 = [0, 0];
+                        	x1[0] = x0[0] - grad[0] * t;
+                        	x1[1] = x0[1] - grad[1] * t;
+		        		}
 		        		// ВЫЧИСЛЯЕМ НОРМУ РАЗНОСТИ КООРДИНАТ
 	                    norm_difference = 0;
 		        		norm_difference = norm2(x0, x1);
@@ -112,41 +124,49 @@ function method_6(obj)
             // alert("точка минимума = [" + result[1].toFixed(5)+", " + result[1].toFixed(5) +"], значение функции в этой точке " + result_function.toFixed(5));
         }
 
-        // Умножение матриц
+        // Умножение матрицы на вектор
 		function MultiplyMatrix(A, B) {
-		    var rowsA = A.length, colsA = A[0].length,
-		        rowsB = B.length, colsB = B[0].length,
+		    var A_l = A.length,
+		        B_l = B.length,
 		        C = [];
-		    if (colsA != rowsB) return false;
-		    for (var i = 0; i < rowsA; i++) C[i] = [];
-		    for (var k = 0; k < colsB; k++) { 
-		    	for (var i = 0; i < rowsA; i++) { 
-		    		var t = 0;
-		          	for (var j = 0; j < rowsB; j++) t += A[i][j]*B[j][k];
-		          	C[i][k] = t;
-		        }
+		    if (A_l != B_l) return false;
+		    for (var i = 0; i < B_l; i++) {
+		    	C[i] = 0;
+		    	for (var j = 0; j < B_l; j++) {
+		    		C[i] += A[i][j] * B[i];
+		    	}
 		    }
 		    return C;
 		}
 
-		// Умножение матрицы на число
-		function multMatrixNumber(a, A) { // a - число, A - матрица (двумерный массив)
-		    var m = A.length, n = A[0].length, B = [];
+		// Умножение вектора на число
+		function multMatrixNumber(a, A) { // a - число, A - вектор
+			var m = A.length, B = [];
 		    for (var i = 0; i < m; i++) { 
 		    	B[i] = [];
-		       	for (var j = 0; j < n; j++) B[i][j] = a*A[i][j];
+		       	B[i] = a * A[i];
 		     }
 		    return B;
 		}
 
-		function step(x, grad){
-            var step = 0;
-            step = (4 * x[0] * grad[0]  +
-                    x[0] * grad[1] + 
-                    x[1] * grad[0] + 
+		function step(x, grad, k, d){
+            var step = 0; 
+            if (k == 0) {
+                step = (4 * x[0] * grad[0] +
+                    x[0] * grad[1] +
+                    x[1] * grad[0] +
                     2 * x[1] * grad[1]) /
-                    (4 * Math.Pow(grad[0], 2) + 
-                    2 * grad[0] * grad[1] + 
-                    2 * Math.Pow(grad[1], 2));
+                    (4 * Math.pow(grad[0], 2) +
+                    2 * grad[0] * grad[1] +
+                    2 * Math.pow(grad[1], 2));
+            } else {
+                step = (4 * x[0] * d[0] +
+                    x[0] * d[1] +
+                    x[1] * d[0] +
+                    2 * x[1] * d[1]) /
+                    (4 * Math.pow(d[0], 2) +
+                    2 * d[0] * d[1] +
+                    2 * Math.pow(d[1], 2));
+            }
             return step;
         }
