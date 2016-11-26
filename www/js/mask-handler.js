348e51a408239277d075bc2e185f023ddf887e59
vlad <vladbeklenyshchev@gmail.com>
2016-11-26 18:05:15 +0200

    Updated input form for function
    Added popover for f(x, y)
    Updated mask method

--- www/js/mask-handler.js
+++ www/js/mask-handler.js
@@ -1,13 +1,17 @@
+function acceptInput() {
+	var sInput = $("#inputFunction").val();
+	try {
+		var slpitedString = sInput.split(' + ');
+		var a = +slpitedString[0][0];
+		var b = +slpitedString[1][0];
+		var c = +slpitedString[2][0];
+		initArgs(a, b, c);
+		confirm('Введена функция:\n'+ sInput);
+	} catch(e) {
+		alert('Введите корректные значения.');
+	}
+}
+
 $(function() {
-	$("#inputFunction").mask("9 x^2 + 9 x y + 9 y^2", {
-		completed: function() { 
-			var sInput = this.val();
-			confirm('Введена функция:\n'+ sInput);
-			var slpitedString = sInput.split(' + ');
-			var a = +slpitedString[0][0];
-			var b = +slpitedString[1][0];
-			var c = +slpitedString[2][0];
-			initArgs(a, b, c);
-		}
-	});
+	$("#inputFunction").mask("9 x ^ 2 + 9 x y + 9 y ^ 2");
 });
