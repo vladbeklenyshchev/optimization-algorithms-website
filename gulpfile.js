079a54c87e32e76011a7f6b27fed4c554b346a7e
vlad <vladbeklenyshchev@gmail.com>
2016-11-26 14:56:53 +0200

    Updated gulpfile (first building was completed)

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp');
-
 var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 
-gulp.task('minify', function () {
-    gulp.src('www/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('build'));
-}); 
+gulp.task('compress', function (cb) {
+    pump([
+    	gulp.src(['www/js/**/*.js', '!www/js/**/*.min.js',
+    		'!www/js/bootstrap.js']),
+    	concat('concat.js'),
+    	gulp.dest('dist/js'),
+    	rename('main.min.js'),
+    	uglify(),
+    	gulp.dest('dist/js')
+    	],
+    	cb
+    );
+});
